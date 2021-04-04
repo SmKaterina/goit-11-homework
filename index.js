296dad5dee68250c4d235149b4bfbbf66ed9819f
SmKaterina <katesemenenko55@gmail.com>
2021-04-04 17:54:41 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const refs = {
+  days: document.querySelector('[data-value="days"]'),
+  hours: document.querySelector('[data-value="hours"]'),
+  minutes: document.querySelector('[data-value="mins"]'),
+  seconds: document.querySelector('[data-value="secs"]'),
+};
+
+const targetDate = new Date(2021, 4, 31, 4, 55);
+
+setInterval(() => {
+  const currentTime = Date.now();
+  const deltaTime = targetDate - currentTime;
+  console.log(updateClockface(deltaTime));
+}, 1000);
+
+const updateClockface = (time) => {
+  const days = Math.floor(time / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+  const secs = Math.floor((time % (1000 * 60)) / 1000);
+  refs.days.textContent = `${days}`;
+  refs.hours.textContent = `${hours}`;
+  refs.minutes.textContent = `${mins}`;
+  refs.seconds.textContent = `${secs}`;
+};
